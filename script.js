2371b597c4c64d3145e6cc026482ec0af16b978a
Anvar Adizov <63946086+Gizmo-30@users.noreply.github.com>
2023-06-14 12:57:42 +0500

    Update script.js

--- script.js
+++ script.js
@@ -80,3 +80,86 @@ const section_3Slide = new SliderMn({
     data_3: 1,
     gap: 32,
 })
+
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+const slider = document.querySelector('.popularItem__slider'),
+    track = document.querySelector('.slider__line'),
+    item = slider.querySelectorAll('.slider__item'),
+    btnPrev = document.querySelector('.slider__button-prev'),
+    btnNext = document.querySelector('.slider__button-next');
+    
+
+
+let position = 0;
+// const slidesToShow = window.innerWidth <= 1024 ? 1 : 4;
+const slidesToShow = 1;
+const slidesToScroll = 1;
+const itemsCount = item.length;
+const gap = 32
+const itemWidth = Math.ceil((slider.clientWidth - gap * (slidesToShow - 1)) / slidesToShow);
+// const itemWidth = slider.clientWidth / slidesToShow;
+// const movePosition = slidesToScroll * itemWidth;
+const movePosition = (slidesToScroll * itemWidth) + (slidesToScroll * gap);
+// const movePosition = slidesToScroll * itemWidth;
+// console.log(movePosition);
+
+item.forEach(element => {
+    element.style = `min-width: ${itemWidth}px;`
+});
+
+btnPrev.addEventListener('click', () => {  
+    const itemsLeft = Math.floor((Math.abs(position) / itemWidth))
+    position += itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth + itemsLeft * gap
+    setPosition()
+})
+
+btnNext.addEventListener('click', () => {
+    // const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth + (slidesToShow * gap)) / itemWidth;
+    // const itemsLeft = itemsCount - (Math.abs(position) + slidesToShow * itemWidth + slidesToShow * gap) / itemWidth;
+    const itemsLeft = itemsCount - Math.floor((Math.abs(position) + slidesToShow * itemWidth - slidesToShow * gap) / itemWidth);
+
+    // console.log(Math.abs(itemsCount));
+    // console.log(Math.abs(position));
+    // console.log(Math.abs(slidesToShow));
+    // console.log(Math.abs(itemWidth));
+    // console.log(Math.abs((slidesToShow * gap)));
+    // console.log(Math.abs((slidesToShow * itemWidth)));
+    // console.log(itemsCount - (Math.abs(position) + slidesToShow * itemWidth + (slidesToShow * gap)));
+    // console.log(itemsLeft);
+
+    position -= itemsLeft >= slidesToScroll ? movePosition : itemsLeft * itemWidth + itemsLeft * gap
+    // position -= movePosition 
+    setPosition()
+})
+
+function setPosition() {
+    track.style = `transform: translateX(${position}px)`
+    checkBtn()
+}
+
+function checkBtn() {
+    btnPrev.disabled = position === 0
+    btnNext.disabled = position <= - (itemsCount - slidesToShow) * itemWidth
+    
+    if (position === 0) {
+        btnPrev.classList.remove('active') 
+    } else {
+        btnPrev.classList.add('active')
+    }
+    
+    if (position <= - (itemsCount - slidesToShow) * itemWidth) {
+        btnNext.classList.remove('active') 
+    } else {
+        btnNext.classList.add('active') 
+    }
+}
+
+checkBtn()
